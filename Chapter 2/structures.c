2a72639485ae28a1ae7663815246aea3574763f2
Sang Han <jjangsangy@gmail.com>
2014-06-25 06:25:31 -0700

    Ch4

--- Chapter 2/structures.c
+++ Chapter 2/structures.c
@@ -1,41 +1,42 @@
-int     nsym    0;      /* current length of symbol table */
+int nsym 0;
 
 struct {
-       char    id[10];
-       int     line;
-       char    type;
-       int     usage;
-} sym[100];             /* symbol table */
+       char id[10];
+       int line;
+       char type;
+       int usage;
+} sym[100];
 
-main( ) {
-       ...
-       if( (index = lookup(newname)) >= 0 )
-               sym[index].usage++;              /* already there ... */
-       else
-               install(newname, newline, newtype);
-       ...
+main() {
+    if( (index = lookup(newname)) >= 0 )
+        sym[index].usage++;
+    else
+        install(newname, newline, newtype);
 }
 
 lookup(s)
-  char *s; {
+  char *s;
        int i;
        extern struct {
-               char    id[10];
-               int     line;
-               char    type;
-               int     usage;
-       } sym[ ];
+           char id[10];
+           int line;
+           char type;
+           int usage;
+       } sym[];
 
-       for( i=0; i<nsym; i++ )
-               if( compar(s, sym[i].id) > 0 )
-                       return(i);
+       for( i=0; i<nsym; i++ ) {
+           if( compar(s, sym[i].id) > 0 )
+               return(i);
+       }
        return(-1);
 }
 
-compar(s1,s2)           /*  return 1 if s1==s2, 0 otherwise */
-  char *s1, *s2; {
-       while( *s1++ == *s2 )
-               if( *s2++ == '\0' )
-                       return(1);
-       return(0);
+compar(s1,s2)
+    char *s1, *s2;
+
+    while(*s1++ == *s2) {
+        if(*s2++ == '\0')
+            return(1);
+    }
+    return(0);
 }
