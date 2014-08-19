077ecd8ace90c833f53c4ff91173b8d85da57de6
Sang Han <jjangsangy@gmail.com>
2014-08-18 23:10:51 -0700

    init

--- Chapter 4/function.c
+++ Chapter 4/function.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
 
-int getline(char line[], int max)
+int getline(char line[], int max);
 int strindex(char source[], char searchfor[]);
 
 char pattern[] = "ould"; /* pattern to search for */
 
 /* find all lines matching pattern */
-main()
+int main(void)
 {
     char line[MAXLINE];
     int found = 0;
---
+++ Chapter 6/keytab.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXWORD 100
+#define NKEYS (sizeof keytab / sizeof(struct key))
+
+struct key {
+    char *word;
+    int count;
+};
+
+struct key keytab[] = {
+    {"auto", 0},
+    {"break", 0},
+    {"case", 0},
+    {"char", 0},
+    {"const", 0},
+    {"continue", 0},
+    {"default", 0},
+    /* ... */
+    {"unsigned", 0},
+    {"void", 0},
+    {"volatile", 0},
+    {"while", 0},
+};
+
+/* binsearch: find word in tab[0]...tab[n-1] */
+int binsearch(char *word, struct key tab[], int n)
+{
+    int cond;
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low+high) / 2;
+        if ((cond = strcmp(word, tab[mid].word)) < 0)
+            high = mid - 1;
+        else if (cond > 0)
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+}
+
+/* getword: get next word or character from input */
+int getword(char *word, int lim)
+{
+    int c, getchar(void);
+    /* int ungetc(int); */
+    char *w = word;
+
+    while (isspace(c = getchar()))
+        ;
+    if (c != EOF)
+        *w++ = c;
+    if (!isalpha(c)) {
+        *w = '\n';
+        return c;
+    }
+    for ( ; --lim > 0; w++)
+        if (!isalnum(*w = getchar())) {
+            ungetc(*w);
+            break;
+        }
+    *w = '\n';
+    return word[0];
+}
+/* count C keywords */
+int main(void)
+{
+    int n;
+    char word[MAXWORD];
+
+    while (getword(word, MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            if ((n = binsearch(word, keytab, NKEYS)) >= 0)
+                keytab[n].count++;
+    for (n = 0; n < NKEYS; n++)
+        if (keytab[n].count > 0)
+            printf("%4d %s\n",
+                    keytab[n].count, keytab[n].word);
+    return 0;
+}
+
